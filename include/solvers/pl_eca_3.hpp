60f1148e576f696d2122cb71b9bce745c155995e
PlaiSeek <francois.hamonic@gmail.com>
2021-04-21 01:36:18 +0200

    fix output

--- include/solvers/pl_eca_3.hpp
+++ include/solvers/pl_eca_3.hpp
@@ -12,7 +12,7 @@ namespace Solvers {
         public:
             PL_ECA_3() {
                 params["log"] = new IntParam(0);
-                params["timeout"] = new IntParam(0);
+                params["timeout"] = new IntParam(3600);
                 params["relaxed"] = new IntParam(0);
                 params["fortest"] = new IntParam(0);
             }
