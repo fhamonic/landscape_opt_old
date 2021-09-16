8385b8bbb13ee8d03567e994b7ac559d8e9d9791
PlaiSeek <francois.hamonic@gmail.com>
2021-09-16 12:20:55 +0200

    thread test

--- exec/ttest.cpp
+++ exec/ttest.cpp
@@ -1,12 +1,29 @@
-#include <filesystem>
-#include <fstream>
+#include <algorithm>
+#include <execution>
 #include <iostream>
+#include <vector>
 
+#include <atomic>
 #include <thread>
 #include "tbb/global_control.h"
 
-int main() {
-    std::cout << "max allowed parallelism : "
-              << std::thread::hardware_concurrency() << std::endl;
+int main(int argc, char ** argv) {
+    if(argc < 2) {
+        std::cout << "Usage: " << argv[0] << " <N>" << std::endl;
+        return EXIT_FAILURE;
+    }
+    const std::size_t n = std::atoi(argv[1]);
+    std::vector<int> v;
+    v.reserve(n);
+    for(int i =0; i< n; ++i) {
+        v.push_back(i);
+    }
+
+    std::atomic<int> sum = 0;
+    std::for_each(std::execution::par_unseq, v.begin(), v.end(),
+                  [&](const auto i) { sum += i; });
+
+    std::cout << sum << std::endl;
+
     return EXIT_SUCCESS;
 }
