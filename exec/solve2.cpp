2dcf01fb3aa99ff30cbf5ae452e9997aef45dcc2
PlaiSeek <francois.hamonic@gmail.com>
2022-02-06 00:23:12 +0100

    solve2 interface ok, TODO: instance2, parse_instance_2

--- exec/solve2.cpp
+++ exec/solve2.cpp
@@ -46,12 +46,10 @@ void init_logging() {
                                      logging::trivial::warning);
 }
 
-static bool process_command_line(int argc, char * argv[],
-                                 std::shared_ptr<concepts::Solver> & solver,
-                                 std::filesystem::path & instances_description_json,
-                                 double & budget,
-                                 bool & output_in_file,
-                                 std::filesystem::path & output_csv) {
+static bool process_command_line(
+    int argc, const char * argv[], std::shared_ptr<concepts::Solver> & solver,
+    std::filesystem::path & instances_description_json_file, double & budget,
+    bool & output_in_file, std::filesystem::path & output_csv_file) {
     std::vector<std::shared_ptr<concepts::Solver>> solvers{
         std::make_unique<Solvers::Bogo>(),
         std::make_unique<Solvers::Naive_ECA_Inc>(),
@@ -72,16 +70,16 @@ static bool process_command_line(int argc, char * argv[],
             "select the solver to use")("list-solvers,ls",
                                         "list the available solvers")(
             "params,p",
-            bpo::value<vector<std::string>>(&solver_params)->multitoken(),
+            bpo::value<std::vector<std::string>>(&solver_params)->multitoken(),
             "set the parameters of the selected solver")(
             "list-params",
             "list the available parameters of the selected solver")(
             "instance,i",
             bpo::value<std::filesystem::path>(&instances_description_json_file)
                 ->required(),
-            "set instance decription json file")("budget,B",
-                                   bpo::value<double>(budget)->required(),
-                                   "set the budget value")(
+            "set instance decription json file")(
+            "budget,B", bpo::value<double>(&budget)->required(),
+            "set the budget value")(
             "output,o", bpo::value<std::filesystem::path>(&output_csv_file),
             "set solution output csv file");
 
@@ -94,32 +92,39 @@ static bool process_command_line(int argc, char * argv[],
         }
         if(vm.count("list-solvers")) {
             std::cout << "Available solvers:" << std::endl;
-            for(auto & p : solvers) std::cout << "\t" << p.first << std::endl;
+            for(auto & s : solvers) std::cout << "\t" << s->name() << std::endl;
             return false;
         }
         bpo::notify(vm);
         auto it = br::find_if(solvers, [&solver_name](auto & s) {
-            return s.name() == solver_name;
+            return s->name() == solver_name;
         });
         if(it == solvers.end())
             throw std::invalid_argument(
                 solver_name +
                 " is not an available solver, see --list-solvers.");
-        solver = it->second;
+        solver = *it;
         if(vm.count("list-params")) {
             std::cout << "Available options for " << solver_name << ":"
                       << std::endl;
-            for(auto & param : solver->getParamList())
-                std::cout << "\t" << param << std::endl;
+            for(auto & param : solver->getParams())
+                std::cout << "\t" << param.first << std::endl;
             return false;
         }
         for(auto param : solver_params) {
-            auto [param_name, param_value] = BPOUtils::split_equality_str(param);
-            bool param_exists = solver->setParam(param_name, param_value.data());
+            auto [param_name, param_value] =
+                BPOUtils::split_equality_str(param);
+            bool param_exists =
+                solver->setParam(param_name, param_value.data());
             if(!param_exists) {
-               std::cout << "Invalid parameter '"<< param <<"' for " << solver_name << ", see:"
-                      << std::endl;
-            return false; 
+                std::cout << "Invalid parameter '" << param << "' for "
+                          << solver_name << ", see --list-params." << std::endl;
+                return false;
+            }
+            if(param_value.empty()) {
+                 std::cout << "Invalid value for parameter '" << param_name << "' of "
+                          << solver_name << std::endl;
+                return false;
             }
         }
         output_in_file = vm.count("output");
@@ -137,64 +142,19 @@ int main(int argc, const char * argv[]) {
     bool output_in_file;
     std::filesystem::path output_csv;
 
-    bool valid_command = process_command_line(
-        argc, argv, solver, instances_description_json, budget, output_in_file, output_csv);
+    bool valid_command =
+        process_command_line(argc, argv, solver, instances_description_json,
+                             budget, output_in_file, output_csv);
     if(!valid_command) return EXIT_FAILURE;
     init_logging();
 
-    std::filesystem::path landscape_path = argv[1];
-    std::filesystem::path problem_path = argv[2];
-    double B = std::atof(argv[3]);
-    std::string solver_name = argv[4];
+    // Instance2 instance = parse_instance(instances_description_json);
 
-    auto it = boost::find_if(solvers, [&solver_name](const auto & solver) {
-        return solver->name() == solver_name;
-    });
+    // Helper::assert_well_formed(instance.landscape, instance.plan);
 
-    if(it == solvers.end()) {
-        std::cerr << "Availables solvers :" << std::endl;
-        for(const auto & solver : solvers)
-            std::cerr << "\t" << solver->name() << std::endl;
-        return EXIT_FAILURE;
-    }
+    // Solution solution = solver.solve(instance.landscape, instance.plan, instance.budget);
 
-    concepts::Solver & solver = std::ref(*it->get());
-
-    for(int i = 5; i < argc; i++) {
-        std::string arg = argv[i];
-        const int split_index = arg.find("=");
-        std::string option = arg.substr(0, split_index);
-        std::string value = arg.substr(split_index + 1, -1);
-
-        if(!solver.setParam(option, value.data())) {
-            auto params = solver.getParams();
-            if(params.empty()) {
-                std::cerr << "No options available for \"" << solver.name()
-                          << "\"" << std::endl;
-            } else {
-                std::cerr << "Available options for \"" << solver.name()
-                          << "\" :" << std::endl;
-                for(const auto & [param_name, param_value] : params)
-                    std::cerr << "\t" << param_name << std::endl;
-            }
-            return EXIT_FAILURE;
-        }
-    }
-
-    std::string name = std::filesystem::path(problem_path).stem();
-
-    MutableLandscape landscape =
-        StdMutableLandscapeParser::get().parse(landscape_path);
-
-    StdRestorationPlanParser parser(landscape);
-    RestorationPlan<MutableLandscape> plan = parser.parse(problem_path);
-
-    // Helper::assert_well_formed(landscape, plan);
-
-    plan.initElementIDs();
-    Solution solution = solver.solve(landscape, plan, B);
-
-    Helper::printSolution(landscape, plan, name, solver, B, solution);
+    // Helper::printSolution(instance.landscape, instance.plan, name, solver, instance.budget, solution);
 
     return EXIT_SUCCESS;
 }
