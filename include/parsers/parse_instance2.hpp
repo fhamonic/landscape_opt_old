f670863f21ae5f47ca4ec32885bad85c2adf1ece
PlaiSeek <francois.hamonic@gmail.com>
2022-02-10 17:27:40 +0100

    instance csv parser

--- include/parsers/parse_instance2.hpp
+++ include/parsers/parse_instance2.hpp
@@ -3,6 +3,7 @@
 
 #include <filesystem>
 #include <fstream>
+#include <initializer_list>
 #include <iostream>
 
 #include "solvers/concept/instance2.hpp"
@@ -10,51 +11,60 @@
 #include <fast-cpp-csv-parser/csv.h>
 #include <nlohmann/json.hpp>
 
-template <typename T>
-void assert_json_property(T json_object, std::string property,
-                          std::string parent = "") {
-    if(!instance_json.contains("options"))
-        throw std::invalid_argument(
-            "No 'file' property" +
-            (parent.empty() ? "" : " in '" + parent + "'"));
-}
-
-std::string json_type_str(nlhomann::detail::value_t type) {
+std::string json_type_str(nlohmann::detail::value_t type) {
     switch(type) {
-        case nlhomann::detail::value_t::null:
+        case nlohmann::detail::value_t::null:
             return "null";
-        case nlhomann::detail::value_t::object:
+        case nlohmann::detail::value_t::object:
             return "object";
-        case nlhomann::detail::value_t::array:
+        case nlohmann::detail::value_t::array:
             return "array";
-        case nlhomann::detail::value_t::string:
+        case nlohmann::detail::value_t::string:
             return "string";
-        case nlhomann::detail::value_t::boolean:
+        case nlohmann::detail::value_t::boolean:
             return "boolean";
-        case nlhomann::detail::value_t::number_integer:
+        case nlohmann::detail::value_t::number_integer:
             return "number_integer";
-        case nlhomann::detail::value_t::number_unsigned:
+        case nlohmann::detail::value_t::number_unsigned:
             return "number_unsigned";
-        case nlhomann::detail::value_t::number_float:
+        case nlohmann::detail::value_t::number_float:
             return "number_float";
-        case nlhomann::detail::value_t::binary:
+        case nlohmann::detail::value_t::binary:
             return "binary";
+        default:
+            throw "unknowned";
     }
 }
 
 template <typename T>
 void assert_json_properties(
     T json_object,
-    std::initializer_list<std::string, nlhomann::detail::value_t> properties) {
+    std::initializer_list<std::pair<std::string, nlohmann::detail::value_t>>
+        properties) {
     for(const auto & [property, type] : properties) {
-        if(!instance_json.contains("options"))
-            throw std::invalid_argument("No 'file' property");
-        if(!instance_json.type() != type)
+        if(!json_object.contains(property))
+            throw std::invalid_argument("No '" + property + "' property");
+        const auto json_property = json_object[property];
+        if(json_property.type() != type)
             throw std::invalid_argument(
-                "Property" + property + " expected to of type '" +
+                "Property '" + property + "' expected of type '" +
                 json_type_str(type) + "' but is '" +
-                json_type_str(instance_json.type()) + "'");
+                json_type_str(json_property.type()) + "'");
     }
+}
+
+template <typename C, typename T, typename... Columns>
+void configure_csv_reader(C & csv, T json_object,
+                          Columns... default_column_names) {
+    auto get_column_name = [&](std::string && c) -> std::string {
+        if(json_object.contains("columns")) {
+            auto columns_json = json_object["columns"];
+            if(columns_json.contains(c)) return columns_json[c];
+        }
+        return c;
+    };
+    csv.read_header(io::ignore_extra_column,
+                    get_column_name(default_column_names)...);
 }
 
 Instance2 parse_instance2(std::filesystem::path instance_path) {
@@ -64,39 +74,69 @@ Instance2 parse_instance2(std::filesystem::path instance_path) {
     nlohmann::json instance_json;
     instance_stream >> instance_json;
 
-    instance_json.type()
-
-        assert_json_property(instance_json, "options");
+    assert_json_properties(instance_json,
+                           {{"options", nlohmann::detail::value_t::object},
+                            {"cases", nlohmann::detail::value_t::array}});
     auto options_json = instance_json["options"];
-    std::filesystem::path options_csv_path = options_json["file"];
-    if(options_csv_path.is_relative())
-        options_csv_path = (instance_path.parent_path() / options_csv_path);
-    std::string options_id_column = "id";
-    std::string options_cost_column = "cost";
-    if(options_json.contains("columns")) {
-        auto options_columns_json = options_json["columns"];
-        if(options_columns_json.contains("id"))
-            options_id_column = options_columns_json["id"];
-        if(options_columns_json.contains("cost"))
-            options_cost_column = options_columns_json["cost"];
+    assert_json_properties(options_json,
+                           {{"file", nlohmann::detail::value_t::string}});
+
+    {
+        std::filesystem::path options_csv_path = options_json["file"];
+        if(options_csv_path.is_relative())
+            options_csv_path = (instance_path.parent_path() / options_csv_path);
+        io::CSVReader<2> options_csv(options_csv_path);
+        configure_csv_reader(options_csv, options_json, "option_id", "option_cost");
+
+        std::string option_id;
+        double option_cost;
+        while(options_csv.read_row(option_id, option_cost)) {
+            if(instance.containsOption(option_id))
+                throw std::invalid_argument("Option identifier '" + option_id +
+                                            "' appears multiple times in ");
+            instance.addOption(option_id, option_cost);
+        }
     }
 
-    io::CSVReader<2> options_csv(options_csv_path);
-    options_csv.read_header(io::ignore_extra_column, options_id_column,
-                            options_cost_column);
+    for(auto case_json : instance_json["cases"]) {
+        assert_json_properties(case_json,
+                               {{"name", nlohmann::detail::value_t::string},
+                                {"nodes", nlohmann::detail::value_t::object},
+                                {"arcs", nlohmann::detail::value_t::object}});
 
-    std::string option_id;
-    double option_cost;
-    while(options_csv.read_row(option_id, option_cost)) {
-        if(instance.containsOption(option_id))
-            throw std::invalid_argument("Option identifier '" + option_id +
-                                        "' appears multiple times in ");
-        instance.addOption(option_id, option_cost);
+        // InstanceCase & instance_case = instance.cases.emplace_back();
+
+        auto nodes_json = case_json["nodes"];
+        std::filesystem::path nodes_csv_path = nodes_json["file"];
+        if(nodes_csv_path.is_relative())
+            nodes_csv_path = (instance_path.parent_path() / nodes_csv_path);
+        io::CSVReader<2> nodes_csv(nodes_csv_path);
+        configure_csv_reader(nodes_csv, nodes_json, "node_id", "node_quality");
+
+        std::string node_id;
+        double node_quality;
+        while(nodes_csv.read_row(node_id, node_quality)) {
+            // if(instance.containsOption(node_id))
+            //     throw std::invalid_argument("Option identifier '" + node_id +
+            //                                 "' appears multiple times in ");
+        }
+
+
+        auto arcs_json = case_json["arcs"];
+        std::filesystem::path arcs_csv_path = arcs_json["file"];
+        if(arcs_csv_path.is_relative())
+            arcs_csv_path = (instance_path.parent_path() / arcs_csv_path);
+        io::CSVReader<2> arcs_csv(arcs_csv_path);
+        configure_csv_reader(arcs_csv, arcs_json, "arc_id", "arc_probability");
+
+        std::string arc_id;
+        double arc_probability;
+        while(arcs_csv.read_row(arc_id, arc_probability)) {
+            // if(instance.containsOption(arc_id))
+            //     throw std::invalid_argument("Option identifier '" + arc_id +
+            //                                 "' appears multiple times in ");
+        }
     }
-
-    if(!instance_json.contains("cases"))
-        throw std::invalid_argument("No 'file' property in 'cases' of " +
-                                    instance_path.string());
 
     return instance;
 }
